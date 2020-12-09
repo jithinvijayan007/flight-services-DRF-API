cc489ffcd3e28bfaa5c75cab6d97f4a59bb76c1a
Jithin <67956333+jithinvijayan007@users.noreply.github.com>
2020-12-09 08:58:18 +0530

    Update views.py

--- flightApp/views.py
+++ flightApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from blog.models import Post, Comment
-from django.utils import timezone
+from django.utils import timezone 
 from blog.forms import PostForm, CommentForm
 
 from django.views.generic import (TemplateView,ListView,
